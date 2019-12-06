fd8767db2e994e3dee3578d492618f547178232f
Jefferson de Guzman <jefferson30.jdg@gmail.com>
2019-12-06 13:15:49 +0800

    Initialization of fetching price

--- src/app/edit-price/edit-price.component.ts
+++ src/app/edit-price/edit-price.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router , ActivatedRoute , ParamMap  } from '@angular/router';
+import { AdminService } from '../admin.service';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-edit-price',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditPriceComponent implements OnInit {
 
-  constructor() { }
+  price;
+  id;
+
+  constructor(private route: ActivatedRoute,
+    private router: Router , public adminService : AdminService,
+    public spinner : NgxSpinnerService) { }
 
   ngOnInit() {
+   
+  	this.route.paramMap.subscribe((params: ParamMap) => {
+      this.id = parseInt(params.get('id'));
+ 		console.log(this.adminService.customersPage['page'+this.adminService.customerspageActive ][this.id])
+ 		this.customer = this.adminService.customersPage['page'+this.adminService.customerspageActive ][this.id];
+    });
   }
 
 }
