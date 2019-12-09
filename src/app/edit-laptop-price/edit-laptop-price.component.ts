2d5028b02ead0936050089626044e07b7065bb9f
Jefferson de Guzman <jefferson30.jdg@gmail.com>
2019-12-09 16:14:06 +0800

    Added functionality

--- src/app/edit-laptop-price/edit-laptop-price.component.ts
+++ src/app/edit-laptop-price/edit-laptop-price.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AdminService } from '../admin.service';
 
 @Component({
   selector: 'app-edit-laptop-price',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditLaptopPriceComponent implements OnInit {
 
-  constructor() { }
+  price;
+
+  constructor(public adminService : AdminService) { }
 
   ngOnInit() {
+        this.adminService.getLaptopPrice().subscribe(res => {
+          console.log(res);
+          this.price = res['result'][0];
+          console.log(this.price);
+        });
+      //     if (res) {
+         
+      //     }
+         
+      //   }, err => {
+         
+      //   });
+
+      // });
+     
   }
 
 }
