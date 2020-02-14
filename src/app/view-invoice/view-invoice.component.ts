849473370117a3180c79f8e08530dc42ace1f7ff
Jefferson de Guzman <jefferson30.jdg@gmail.com>
2020-02-14 16:22:49 +0800

    Fixed invoice repair

--- src/app/view-invoice/view-invoice.component.ts
+++ src/app/view-invoice/view-invoice.component.ts
@@ -42,40 +42,40 @@ export class ViewInvoiceComponent implements OnInit {
     this.adminService.getInvoice(this.id).subscribe(res => {
       console.log(res);
       this.invoice = res;
+      this.adminService.getRepair(this.invoice.bookings_id).subscribe(res => {
+      console.log(JSON.stringify(res));
+      this.repair = res;
+      if (this.repair.screenrep_selected == 1)
+        this.selectedRepairs.push("Screen Replacement");
+      if (this.repair.headrep_selected == 1)
+        this.selectedRepairs.push("Headphone Repair");
+      if (this.repair.earrep_selected == 1)
+        this.selectedRepairs.push("Ear Piece Repair");
+      if (this.repair.powerrep_selected == 1)
+        this.selectedRepairs.push("Power Button Repair");
+      if (this.repair.rearcamrep_selected == 1)
+        this.selectedRepairs.push("Rear Camera Repair");
+      if (this.repair.frontcamrep_selected == 1)
+        this.selectedRepairs.push("Front Camera Repair");
+      if (this.repair.homerep_selected == 1)
+        this.selectedRepairs.push("Home Button Repair");
+      if (this.repair.microphone_selected == 1)
+        this.selectedRepairs.push("Microphone Repair");
+      if (this.repair.chargeport_selected == 1)
+        this.selectedRepairs.push("Charging Port Repair");
+      if (this.repair.volumerep_selected == 1)
+        this.selectedRepairs.push("Volume Repair");
+      if (this.repair.battrep_selected == 1)
+        this.selectedRepairs.push("Battery Replacement");
+      if (this.repair.signalrep_selected == 1)
+        this.selectedRepairs.push("Cellular Signal Repair");
+      if (this.repair.backglassrep_selected == 1)
+        this.selectedRepairs.push("Back Glass Replacement");
+
+      console.log(JSON.stringify(this.repair));
+
     })
-    // this.adminService.getRepair(this.bookingId).subscribe(res => {
-    //   console.log(JSON.stringify(res));
-    //   this.repair = res;
-    //   if (this.repair.screenrep_selected == 1)
-    //     this.selectedRepairs.push("Screen Replacement");
-    //   if (this.repair.headrep_selected == 1)
-    //     this.selectedRepairs.push("Headphone Repair");
-    //   if (this.repair.earrep_selected == 1)
-    //     this.selectedRepairs.push("Ear Piece Repair");
-    //   if (this.repair.powerrep_selected == 1)
-    //     this.selectedRepairs.push("Power Button Repair");
-    //   if (this.repair.rearcamrep_selected == 1)
-    //     this.selectedRepairs.push("Rear Camera Repair");
-    //   if (this.repair.frontcamrep_selected == 1)
-    //     this.selectedRepairs.push("Front Camera Repair");
-    //   if (this.repair.homerep_selected == 1)
-    //     this.selectedRepairs.push("Home Button Repair");
-    //   if (this.repair.microphone_selected == 1)
-    //     this.selectedRepairs.push("Microphone Repair");
-    //   if (this.repair.chargeport_selected == 1)
-    //     this.selectedRepairs.push("Charging Port Repair");
-    //   if (this.repair.volumerep_selected == 1)
-    //     this.selectedRepairs.push("Volume Repair");
-    //   if (this.repair.battrep_selected == 1)
-    //     this.selectedRepairs.push("Battery Replacement");
-    //   if (this.repair.signalrep_selected == 1)
-    //     this.selectedRepairs.push("Cellular Signal Repair");
-    //   if (this.repair.backglassrep_selected == 1)
-    //     this.selectedRepairs.push("Back Glass Replacement");
-
-    //   console.log(JSON.stringify(this.repair));
-
-    // })
+    })
   }
 
   goToBooking() {
