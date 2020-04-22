9ce0d2a55d873502633fd8d891639e253d101f56
Jefferson de Guzman <jefferson30.jdg@gmail.com>
2020-04-22 14:59:29 +0800

    Added Add Device Type Page functionality

---
+++ src/app/add-devtype/add-devtype.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-devtype',
+  templateUrl: './add-devtype.component.html',
+  styleUrls: ['./add-devtype.component.scss']
+})
+export class AddDevtypeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
