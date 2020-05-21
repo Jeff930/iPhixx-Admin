f2ed8793270459a685deecdf743deec02b85b95d
Jefferson de Guzman <jefferson30.jdg@gmail.com>
2020-05-21 14:29:51 +0800

    Added Add Brand component functionality

--- src/app/add-brand/add-brand.component.ts
+++ src/app/add-brand/add-brand.component.ts
@@ -1,4 +1,10 @@
+import * as $ from 'jquery';
 import { Component, OnInit } from '@angular/core';
+import { Router , ActivatedRoute , ParamMap  } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { AdminService } from '../admin.service';
+import { NgxSpinnerService } from 'ngx-spinner';
+
 
 @Component({
   selector: 'app-add-brand',
@@ -7,9 +13,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddBrandComponent implements OnInit {
 
-  constructor() { }
+  locations: any;
 
-  ngOnInit() {
+  constructor(  private route: ActivatedRoute,
+    private router: Router , public adminService : AdminService,
+    public spinner : NgxSpinnerService) { 
+     }
+  
+    brand = { 
+      brand_name: '',
+      brand_path :'',
+      brand_file:''
+    };
+  
+    id ;
+  
+    ngOnInit() {} 
+  
+    addBrand(){
+      console.log("called");
+      console.log(this.brand);
+      this.spinner.show();
+      this.adminService.addBrand(this.brand).subscribe(res => {
+        console.log("this" + res)
+        this.spinner.hide();
+        this.adminService.brandsPage  = new Object(); 
+        this.router.navigate(['/brands']);
+      },
+      (err)=>{
+         console.log(err);
+         alert('Error! Please Try again.')
+         this.spinner.hide();
+      }
+    )
   }
-
-}
+  
+  
+    goToBrand(){
+    this.router.navigate(['/brands']);
+    }
+  
+  }
