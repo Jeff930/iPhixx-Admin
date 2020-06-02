a82427fc1ae08bd67c948caf3420b6024f2a12d1
Jefferson de Guzman <jefferson30.jdg@gmail.com>
2020-06-03 01:29:53 +0800

    Added devtype functionality

--- src/app/add-devtype/add-devtype.component.ts
+++ src/app/add-devtype/add-devtype.component.ts
@@ -1,4 +1,10 @@
+import * as $ from 'jquery';
 import { Component, OnInit } from '@angular/core';
+import { Router , ActivatedRoute , ParamMap  } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { AdminService } from '../admin.service';
+import { NgxSpinnerService } from 'ngx-spinner';
+
 
 @Component({
   selector: 'app-add-devtype',
@@ -7,9 +13,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddDevtypeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+    private router: Router , public adminService : AdminService,
+    public spinner : NgxSpinnerService) { }
 
   ngOnInit() {
+  }
+
+  devtype = { 
+    devtype_name: '',
+    devtype_path :'',
+    devtype_file:''
+  };
+
+  addDevtype(){
+    console.log("called");
+    console.log(this.devtype);
+    this.spinner.show();
+    this.adminService.addDevtype(this.devtype).subscribe(res => {
+      console.log("this" + res)
+      this.spinner.hide();
+      this.adminService.devicesPage  = new Object(); 
+      this.router.navigate(['/devices']);
+    },
+    (err)=>{
+       console.log(err);
+       alert('Error! Please Try again.')
+       this.spinner.hide();
+    }
+  )
+}
+
+goToDevices(){
+  this.router.navigate(['/devices']);
   }
 
 }
