c3eb4da00b5cbd0315ae8cfbad88193fe38f068e
Jefferson de Guzman <jefferson30.jdg@gmail.com>
2020-06-03 22:13:33 +0800

    Added functionality to fetch data from database

--- src/app/add-model/add-model.component.ts
+++ src/app/add-model/add-model.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router , ActivatedRoute , ParamMap  } from '@angular/router';
+import { AdminService } from '../admin.service';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-add-model',
@@ -7,9 +10,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddModelComponent implements OnInit {
 
-  constructor() { }
+  devtypes;
+  brands;
+  id;
 
-  ngOnInit() {
+  constructor(private route: ActivatedRoute,
+    private router: Router , public adminService : AdminService,
+    public spinner : NgxSpinnerService) { }
+
+    ngOnInit() {
+      this.adminService.getDevtypesList().subscribe(res => {
+        this.devtypes = res;
+        this.adminService.getBrandsList().subscribe(res => {
+          this.brands = res.brands;
+        });
+      });
+    }
+  
+    getBrand(brand){
+      console.log(brand);
+      if (brand=='1')
+        return "iPhone";
+      if (brand=='2')
+        return "Samsung";
+      if (brand=='3')
+        return "Huawei";
+      if (brand=='4')
+        return "Sony";
+      if (brand=='5')
+        return "Nokia";
+      if (brand=='6')
+        return "iPad";
+      if (brand=='7')
+        return "Hewlett Packard";
+      if (brand=='8')
+        return "Lenovo";
+      if (brand=='9')
+        return "Dell";
+      if (brand=='10')
+        return "Asus";
+      if (brand=='11')
+        return "Acer";
+      if (brand=='12')
+        return "Microsoft";
+      if (brand=='13')
+        return "Chromebook";
+      if (brand=='14')
+        return "Toshiba";
+      if (brand=='15')
+        return "MacBook";
+    }
+  
+    goToDevices(){
+      this.router.navigate(['/devices']);
+    }
+  
+    updatePrice(){
+      console.log(this.brands);
+      this.adminService.updatePrice(this.brands).subscribe(res => {
+        console.log(res);
+        this.ngOnInit();
+      });
+    }
+  
   }
-
-}
