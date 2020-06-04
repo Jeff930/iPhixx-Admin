6e352cea86e0be223910eeb187c60039b92da0e2
Jefferson de Guzman <jefferson30.jdg@gmail.com>
2020-06-05 00:02:50 +0800

    Added functionality for Edit Brand

--- src/app/edit-brand/edit-brand.component.ts
+++ src/app/edit-brand/edit-brand.component.ts
@@ -1,4 +1,9 @@
+import * as $ from 'jquery';
 import { Component, OnInit } from '@angular/core';
+import { Router , ActivatedRoute , ParamMap  } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { AdminService } from '../admin.service';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-edit-brand',
@@ -7,9 +12,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditBrandComponent implements OnInit {
 
-  constructor() { }
+  brand = { 
+  	devicebrand_id: '',
+    device_brand :'',
+  };
+
+  id;
+
+  constructor(private route: ActivatedRoute,
+    private router: Router , public adminService : AdminService,
+    public spinner : NgxSpinnerService) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      this.id = parseInt(params.get('id'));
+        this.adminService.getBrand(this.id).subscribe(res => {
+          console.log(res);
+          if (res) {
+              this.brand = res;
+          }
+         });
+    });
+  }
+
+  updateBrand(){
+  	this.spinner.show();
+    this.adminService.updateDevtype(this.brand).subscribe(res => {
+			console.log(res)
+			this.spinner.hide();
+			this.adminService.brandsPage  = new Object(); 
+	  		this.router.navigate(['/devices']);
+		},
+	 	(err)=>{
+	   		console.log(err);
+	   		alert('Error! Please Try again.')
+	   		this.spinner.hide();
+	  	}
+	)
+  }
+
+  goToDevices(){
+    this.router.navigate(['/devices']);
   }
 
 }
