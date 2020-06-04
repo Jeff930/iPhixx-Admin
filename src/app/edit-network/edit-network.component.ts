fa5f66b86f6f8c84923afa4f145c5754ceea9beb
Jefferson de Guzman <jefferson30.jdg@gmail.com>
2020-06-05 00:45:52 +0800

    Added contents for Edit Network Page

--- src/app/edit-network/edit-network.component.ts
+++ src/app/edit-network/edit-network.component.ts
@@ -1,4 +1,9 @@
+import * as $ from 'jquery';
 import { Component, OnInit } from '@angular/core';
+import { Router , ActivatedRoute , ParamMap  } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { AdminService } from '../admin.service';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-edit-network',
@@ -7,9 +12,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditNetworkComponent implements OnInit {
 
-  constructor() { }
+  network = { 
+  	carrier_no: '',
+    carrier_name :'',
+  };
+
+  id;
+
+  constructor(private route: ActivatedRoute,
+    private router: Router , public adminService : AdminService,
+    public spinner : NgxSpinnerService) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      this.id = parseInt(params.get('id'));
+        this.adminService.getNetwork(this.id).subscribe(res => {
+          console.log(res);
+          if (res) {
+              this.network = res;
+          }
+         });
+    });
+  }
+
+  updateBrand(){
+  	this.spinner.show();
+    this.adminService.updateNetwork(this.network).subscribe(res => {
+			console.log(res)
+			this.spinner.hide();
+			this.adminService.networksPage  = new Object(); 
+	  		this.router.navigate(['/networks']);
+		},
+	 	(err)=>{
+	   		console.log(err);
+	   		alert('Error! Please Try again.')
+	   		this.spinner.hide();
+	  	}
+	)
+  }
+
+  goToNetworks(){
+    this.router.navigate(['/networks']);
   }
 
 }
