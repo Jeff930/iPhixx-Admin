d55cb342dcf5b3db20fd310752e29a0340af75ff
Jefferson de Guzman <jefferson30.jdg@gmail.com>
2020-06-16 16:25:57 +0800

    Added functionality to accept images

--- src/app/edit-brand/edit-brand.component.ts
+++ src/app/edit-brand/edit-brand.component.ts
@@ -14,8 +14,13 @@ export class EditBrandComponent implements OnInit {
 
   brand = { 
   	devicebrand_id: '',
-    device_brand :'',
+    device_brand :''
   };
+
+  brand_file:'';
+
+  imagePath=null;
+  file:File;
 
   id;
 
@@ -38,6 +43,25 @@ export class EditBrandComponent implements OnInit {
   updateBrand(){
   	this.spinner.show();
     this.adminService.updateBrand(this.brand).subscribe(res => {
+      if (this.imagePath==null){
+        console.log(res);
+        this.spinner.hide();
+        this.adminService.brandsPage  = new Object(); 
+        this.router.navigate(['/devices']);
+      }else{
+        this.adminService.uploadBrandImage(this.file,res['device_brand']).subscribe(
+          (res) => {
+            console.log(res);
+            this.spinner.hide();
+            this.adminService.brandsPage  = new Object(); 
+            this.router.navigate(['/devices']);
+          },
+          (err) => {
+            console.log(err);
+            alert('Error! Please Try again.')
+            this.spinner.hide();
+          })
+      } 
 			console.log(res)
 			this.spinner.hide();
 			this.adminService.brandsPage  = new Object(); 
@@ -49,6 +73,21 @@ export class EditBrandComponent implements OnInit {
 	   		this.spinner.hide();
 	  	}
 	)
+  }
+
+  acceptImage(image){
+    console.log(image);
+    this.file = image.files[0];
+    console.log(this.file);
+    const reader = new FileReader();
+    console.log(this.file);
+    reader.addEventListener('load', (event: any) => {
+      this.imagePath = event.target.result;
+      this.file = this.imagePath;
+      console.log(this.imagePath); 
+    });
+  
+    reader.readAsDataURL(this.file);
   }
 
   goToDevices(){
