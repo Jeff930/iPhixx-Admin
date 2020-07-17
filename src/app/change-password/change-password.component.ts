17010e949a6441bbf13ad57c689926d122ca6773
Jeff930 <jefferson30.jdg@gmail.com>
2020-07-17 23:27:35 +0800

    Added change Password functionality

--- src/app/change-password/change-password.component.ts
+++ src/app/change-password/change-password.component.ts
@@ -1,4 +1,9 @@
+import * as $ from 'jquery';
 import { Component, OnInit } from '@angular/core';
+import { Router , ActivatedRoute , ParamMap  } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { AdminService } from '../admin.service';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-change-password',
@@ -7,9 +12,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChangePasswordComponent implements OnInit {
 
-  constructor() { }
+  title;
+  locations: any;
+	
+  constructor(  private route: ActivatedRoute,
+  private router: Router , public adminService : AdminService,
+  public spinner : NgxSpinnerService) { 
+  	//this.adminService.get().subscribe( res => console.log(res))
+ 
+   }
+
+  password = { 
+  	current_password: '',
+	  new_password :'',
+    confirm_password:'',
+    id:0
+  };
+
+  id ;
 
   ngOnInit() {
+  		this.route.paramMap.subscribe((params: ParamMap) => {
+        this.password.id = parseInt(params.get('id'));
+        
+    	});
+   	
+  } 
+  
+  updatePassword(){
+  	this.spinner.show();
+    this.adminService.updatePassword(this.password).subscribe(res => {
+			console.log(res)
+			this.spinner.hide();
+			this.adminService.agentsPage  = new Object(); 
+	  		this.router.navigate(['/leads']);
+		},
+	 	(err)=>{
+	   		console.log(err);
+	   		alert('Error! Please Try again.')
+	   		this.spinner.hide();
+	  	}
+	)
+  }
+
+  goToAgent(){
+	this.router.navigate(['/leads']);
   }
 
 }
