7fc4fee5907d21fa4e4fa7255ec4955872962e40
Jeff930 <jefferson30.jdg@gmail.com>
2020-07-17 23:26:36 +0800

    Added Add Item functionality

--- src/app/add-item/add-item.component.ts
+++ src/app/add-item/add-item.component.ts
@@ -1,4 +1,9 @@
+import * as $ from 'jquery';
 import { Component, OnInit } from '@angular/core';
+import { Router , ActivatedRoute , ParamMap  } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { AdminService } from '../admin.service';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-add-item',
@@ -7,9 +12,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddItemComponent implements OnInit {
 
-  constructor() { }
+  locations: any;
 
-  ngOnInit() {
+  constructor(  private route: ActivatedRoute,
+    private router: Router , public adminService : AdminService,
+    public spinner : NgxSpinnerService) { 
+     }
+  
+    agent = { 
+      agent_fname: '',
+      agent_lname :'',
+      agent_username:'',
+      email : '',
+      phone : '',
+      address :'',
+      location_id : '',
+      password : '',
+    };
+  
+    id ;
+  
+    ngOnInit() { 
+      this.adminService.getLocationList().subscribe( ( res ) => {
+        console.log(res);
+        this.locations = res;
+      })
+    } 
+  
+    addAgent(){
+      console.log("called");
+      console.log(this.agent);
+      this.spinner.show();
+      this.adminService.addAgent(this.agent).subscribe(res => {
+        console.log("this" + res)
+        this.spinner.hide();
+        this.adminService.agentsPage  = new Object(); 
+        this.router.navigate(['/agents']);
+      },
+      (err)=>{
+         console.log(err);
+         alert('Error! Please Try again.')
+         this.spinner.hide();
+      }
+    )
   }
-
-}
+  
+  
+    goToInventory(){
+    this.router.navigate(['/inventory/invt']);
+    }
+  
+  }
+  
